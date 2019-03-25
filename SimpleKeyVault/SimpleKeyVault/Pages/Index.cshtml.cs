4ef12b1e522dc9049f6e66fa8c908b7bcccfd08d
Eyal Schwartz <eyals@microsoft.com>
2019-03-25 16:48:18 -0700

    simple keyvault example

---
+++ SimpleKeyVault/SimpleKeyVault/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+
+
+namespace SimpleKeyVault.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IConfiguration AppConfig { get; set; }
+        public IndexModel(IConfiguration appconfig)
+        {
+            AppConfig = appconfig;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
