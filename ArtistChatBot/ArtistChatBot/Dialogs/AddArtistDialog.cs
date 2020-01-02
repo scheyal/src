efa8bb677ba15ab07536cf1e220320f787c0973e
Eyal Schwartz <eyals@microsoft.com>
2020-01-02 15:38:00 -0800

    progress on addartistdialog

--- ArtistChatBot/ArtistChatBot/Dialogs/AddArtistDialog.cs
+++ ArtistChatBot/ArtistChatBot/Dialogs/AddArtistDialog.cs
@@ -8,6 +8,7 @@ using Microsoft.Bot.Builder.Dialogs.Adaptive.Input;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Templates;
 using Microsoft.Bot.Builder.LanguageGeneration;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -27,6 +28,8 @@ namespace ArtistChatBot
             string[] paths = { ".", "Dialogs", "AddArtistDialog.lg" };
             string fullPath = Path.Combine(paths);
             AppConfig = Configuration;
+            string apiUrl = AppConfig["ApiUrl"] + "/Musician/CreateOrUpdate/";
+            string authKey = AppConfig["ApiAuthKey"];
 
 
             // Create instance of adaptive dialog. 
@@ -41,8 +44,13 @@ namespace ArtistChatBot
                     new OnBeginDialog() // consider parameters
                     {
                         Actions = new List<Dialog> ()
-                        {                            
+                        {
                             new SendActivity("@{WelcomeAddArtist()}"),
+                            new TextInput()
+                            {
+                                Prompt = new ActivityTemplate("@{AskForName()}"),
+                                Property = "user.userProfile.Name"
+                            },
                             new TextInput()
                             {
                                 Property = "turn.Artist",
@@ -87,8 +95,45 @@ namespace ArtistChatBot
                             },
                             RootDialog.DebugAction("Future: use Add Review: [name]"),
 
-                            new SendActivity("@{SubmissionRecord()}"), 
+                            new SendActivity("@{SubmissionRecord()}"),
+                            new ConfirmInput()
+                            {
+                                Property = "turn.ConfirmSubmitArtist",
+                                Prompt = new ActivityTemplate("@{ConfirmPromptSubmitArtist()}")
+                            },
+                            new IfCondition()
+                            {
+                                // All conditions are expressed using the common expression language.
+                                // See https://github.com/Microsoft/BotBuilder-Samples/tree/master/experimental/common-expression-language to learn more
+                                Condition = "turn.ConfirmSubmitArtist == true",
+                                Actions = new List<Dialog>()
+                                {
+                                    new SendActivity("Submitting artist **@{conversation.Artist}** ..."),
+                                    new CodeAction(GenerateArtistBody),
+                                    new Microsoft.Bot.Builder.Dialogs.Adaptive.Actions.HttpRequest()
+                                    {
+                                        // Set response from the http request to turn.httpResponse property in memory.
+                                        ResultProperty = "turn.httpResponse",
+                                        Url = apiUrl,
+                                        Method = HttpRequest.HttpMethod.POST,
+                                        Headers = new Dictionary<string,string> ()
+                                        {
+                                            { "AuthKey", authKey }
+                                        },
 
+                                        Body = "{turn.httpBody}",
+                                    },
+                                    RootDialog.DebugAction("BP @HttpAction"),
+                                    new SendActivity("Submitted. (Result = @{turn.httpResponse}.)"),
+                                    new EndDialog()
+                                },
+                                ElseActions = new List<Dialog>()
+                                {
+                                    new SendActivity("Submit aborted (you can still submit by typing 'submit artist'. Additional commands delete, reset, edit artist (Future or Never)")
+                                }
+                                // We do not need to specify an else block here since if user said no,
+                                // the control flow will automatically return to the last active step (if any)
+                            }
                         },
                     },
                     // Add intents
@@ -117,6 +162,93 @@ namespace ArtistChatBot
         }
 
 
+        private async Task<DialogTurnResult> GenerateArtistBody(DialogContext dc, System.Object options)
+
+        {
+            string artist = dc.GetState().GetValue<string>("conversation.Artist");
+            string album = dc.GetState().GetValue<string>("conversation.Album");
+            string song  = dc.GetState().GetValue<string>("conversation.Song");
+            string review = dc.GetState().GetValue<string>("conversation.Review");
+            string submitter = dc.GetState().GetValue<string>("user.userProfile.Name");
+
+            submitter = String.IsNullOrEmpty(submitter) ? "Anonymous" : submitter;
+
+            string JsonBody = ArtistSubmissionBody(artist, album, song, review, submitter);
+
+            dc.GetState().SetValue("turn.httpbody", JsonBody);
+
+            return new DialogTurnResult(DialogTurnStatus.Complete, options);
+        }
+
+            /// <summary>
+            /// Produces Json for artist submission body.
+            /// Example:
+            /// {
+            //  "Name": "john bonham",
+            //  "Properties": {
+            //    "FavoriteAlbums": [
+            //      {
+            //        "Name": "led zeppelin 1",
+            //        "Votes": "1",
+            //        "Users": "eyal"
+            //      }
+            //    ],
+            //    "FavoriteSongs": [
+            //      {
+            //        "Name": "rocknroll",
+            //        "Votes": "1",
+            //        "Users": "eyal"
+            //      }
+            //    ],
+            //    "Reviews": "that's the way I like it"
+            //  },
+            //  "Votes": "1"
+            //}
+            /// </summary>
+            /// <param name="artist"></param>
+            /// <param name="album"></param>
+            /// <param name="song"></param>
+            /// <param name="review"></param>
+            /// <param name="submmiter"></param>
+            /// <returns></returns>
+            private static string ArtistSubmissionBody(string artist, string album, string song, string review, string submmiter)
+        {
+            string body = string.Empty;
+
+            JObject JB = new JObject(
+                new JProperty("Name", artist),
+                new JProperty("Properties",
+                    new JObject(
+                       new JProperty("FavoriteAlbums",
+                            new JArray(
+                                new JObject(
+                                    new JProperty("Name", album),
+                                    new JProperty("Votes", "1"),
+                                    new JProperty("Users", submmiter)
+                                )
+                            )
+                       ),
+                        new JProperty("FavoriteSongs",
+                            new JArray(
+                                new JObject(
+                                    new JProperty("Name", song),
+                                    new JProperty("Votes", "1"),
+                                    new JProperty("Users", submmiter)
+                               )
+                            )
+                       ),
+                        new JProperty("Reviews",
+                            new JValue(review)
+                       )
+                   )
+                ),
+                new JProperty("Votes", "1")
+            );
+
+            body = JB.ToString();
+
+            return body;
+        }
     }
 }
 
