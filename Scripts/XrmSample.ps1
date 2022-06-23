b72e11fc31f850321a94cffcabfd52f513a223b1
Eyal Schwartz <eyals@microsoft.com>
2022-06-22 17:58:13 -0700

    Xrm powershell sample

---
+++ Scripts/XrmSample.ps1
@@ -0,0 +1,12 @@
+﻿
+# Provisioning
+# Set-ExecutionPolicy -ExecutionPolicy ByPass
+# Install-Module -Name Microsoft.Xrm.Tooling.CrmConnector.PowerShell
+# Update-Module -Name Microsoft.Xrm.Tooling.CrmConnector.PowerShell
+# Get-Help “Crm”
+
+# $Cred = Get-Credential
+# $CRMOrgs = Get-CrmOrganizations -Credential $Cred -DeploymentRegion NorthAmerica –OnlineType Office365
+# $CRMOrgs | get-member  # get all members
+
+$CRMOrgs | Sort-Object FriendlyName | Format-table -AutoSize -Property FriendlyName, WebApplicationUrl 
