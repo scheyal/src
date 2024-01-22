970ed6d8fb2a50b5845114e2cc05c3029f626165
Eyal Schwartz <eyals@microsoft.com>
2024-01-21 20:23:26 -0800

    minor text beautifying

---
+++ Activity3PService/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Activity3PService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Activity3PService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET api/<ActivityController>
+        [HttpGet]
+        public ActivityResponseModel Get()
+        {
+            return new ActivityResponseModel(StatusCodes.Status200OK, "Test OK!");
+        }
+    }
+}
--- Activity3PService/Pages/Index.cshtml.cs
+++ Activity3PService/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,17 @@ namespace Activity3PService.Pages
 {
     public class IndexModel : PageModel
     {
+        public string ServerURL { get; set; }
+        public string TestURL { get; set; }
+        public IndexModel()
+        {
+            TestURL = ServerURL = "[Unavailable]";
+
+        }
         public void OnGet()
         {
+            ServerURL = HttpContext.Request.GetEncodedUrl();
+            TestURL = ServerURL + "api/Test";
         }
     }
 }
